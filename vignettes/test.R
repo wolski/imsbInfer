5c73293028b2ecaad2a28c3808635e5f894c44a1
Witold <wewolski@gmail.com>
2014-05-27 10:23:08 +0200

    [DOC]:

--- vignettes/test.R
+++ vignettes/test.R
@@ -3,76 +3,87 @@ SpecLib = ("/home//witold/Analysis/EBhardt//data/E1404301658-sample-SpecLib/feat
 xx = fread(SpecLib)
 
 colnames(xx)
-
-
 library(imsbInfer)
 data(feature_alignment_requant)
-
 dim(feature_alignment_requant)
+
+
 far =feature_alignment_requant
-far[13,grep("aggr",names(far))]
 
 
-ids = as.character(far$transition_group_id)
-apa = as.character(far$aggr_Peak_Area)
-afa = as.character(far$aggr_Fragment_Annotation)
-
-orig = basename(as.character(far$align_origfilename))
-
-transints = lapply(apa,function(x){unlist(strsplit(x,";"))})
-transids = lapply(afa,function(x){unlist(strsplit(x,";"))})
-
-idss = list(length(transids))
-origf = list(length(transids))
-
-for(i in 1:length(transids)){
-  l=  length(transids[[i]])
-  idss[[i]] = rep(ids[i],l) 
-  origf[[i]] = rep(orig[i],l)
+transitions2wide = function(far){
+  ids = as.character(far$transition_group_id)
+  apa = as.character(far$aggr_Peak_Area)
+  afa = as.character(far$aggr_Fragment_Annotation)
+  orig = basename(as.character(far$align_origfilename))
+  
+  # split transition intensities
+  transints = lapply(apa,function(x){unlist(strsplit(x,";"))})
+  # split transition names
+  transids = lapply(afa,function(x){unlist(strsplit(x,";"))})
+  
+  # prapre output
+  lx = length(transids)
+  idss = list(lx)
+  origf = list(lx)
+  
+  # extend
+  for(i in 1:lx){
+    l =  length(transids[[i]])
+    idss[[i]] = rep(ids[i],l) # transition group ids
+    origf[[i]] = rep(orig[i],l) # orig file name
+  }
+  library(reshape2)
+  # extend to wide format
+  tmp = data.frame(transition_group_id = as.character(unlist(idss)),
+                   align_origfilename = as.character(unlist(origf)),
+                   aggr_Peak_Area = as.numeric(unlist(transints)),
+                   aggr_Fragment_Annotation =  as.character(unlist( transids) ) )
+  #got wide format here...
+  data = dcast(tmp, transition_group_id + aggr_Fragment_Annotation ~ align_origfilename , value.var="aggr_Peak_Area")
+  return(data)
 }
 
-library(reshape2)
+data = transitions2wide(xx)
+
+##### selecting top 2-n transtions ####
 library(data.table)
 
-tmp = data.frame(transition_group_id = as.character(unlist(idss)),
-                 align_origfilename = as.character(unlist(origf)),
-                 aggr_Peak_Area = as.numeric(unlist(transints)),
-                 aggr_Fragment_Annotation =  as.character(unlist( transids) ) )
-#got wide format here...
-data = dcast(tmp, transition_group_id + aggr_Fragment_Annotation ~ align_origfilename , value.var="aggr_Peak_Area")
-dim(data)
-
-medxx = apply(xxvalues,1,median)
-xxmex= cbind( xx[1:2] , medxx)
+medxx = apply(data[,3:dim(data)[2]],1,median)
+xxmex= cbind( data[1:2] , medxx)
 tmpdt = data.table(xxmex)
+# fixing column types
 tmpdt <- tmpdt[, transition_group_id:=as.character(transition_group_id)]
 tmpdt <- tmpdt[, aggr_Fragment_Annotation:=as.character(aggr_Fragment_Annotation)]
+
 setkey(tmpdt , transition_group_id,aggr_Fragment_Annotation)
 
 
-transid = unique(as.character(tmpdt$transition_group_id))
-length(transid)
+transid = unique( as.character(tmpdt$transition_group_id) )
+length( transid )
 
-#res <- vector(length(transid),mode="list")
-nrt=2
+## res = vector(length(transid),mode="list")
+nrt=3
 res <- matrix("",nrow=length(transid)*nrt,ncol=3)
 dim(res)
+start = 1
+end = 0
 for(i in 1:length(transid)){
   tmp<-tmpdt[transid[i],]
-  xx=which(ord(tmp$medxx) < 3)
-  end = nrt*i
-  start = (end-nrt+1)
+  xx=which(order(as.numeric(tmp$medxx),decreasing=TRUE) < (nrt+1))
+  end = start + (length(xx)-1)
   cat("start",start,"end",end,"\n")
-  print(dim(res))
   xbla = as.matrix(tmp[xx,])
   res[start:end,] = xbla
+  start = end + 1
 }
+res = res[1:end,]
 
-colanems(res) = colnames(tmpdt)
+colnames(res) = colnames(tmpdt)
+dim(res)
+head(res)
 
+res = data.table(res)
+res
 
-xx = unlist(res)
-xx[1:12]
-xx = matrix(xx,ncol=3,byrow=F)
-head(xx)
 
