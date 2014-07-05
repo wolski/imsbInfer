4df504f6bf9abb336d0daed22f9db784c5089cb1
unknown <wewolski@google.com>
2014-07-05 22:40:49 +0200

    [FIX] fixed row sorting in aggregateProteins

--- R/aggregateMSexperiment.R
+++ R/aggregateMSexperiment.R
@@ -52,9 +52,8 @@ aggregatePeptides=function(msexp, FUN = sum)
 #' x = removeDecoys( x )
 #' table( table( x$pepinfo$ProteinName ) )
 #' y = aggregateProteins( x )
-#' stopifnot(rownames(y$pepinfo) == rownames(y$Intensity))
+#' stopifnot(rownames(y$pepinfo$ProteinName) == rownames(y$Intensity))
 #' stopifnot( table( table( y$pepinfo$ProteinName ) ) == 50 )
-#' 
 #' 
 #' @seealso aggregatePeptide
 aggregateProteins=function( msexp, FUN = sum )
@@ -65,22 +64,24 @@ aggregateProteins=function( msexp, FUN = sum )
     return(as.matrix(x))
   }
   res = msexp
-  sum(rownames(msexp$Intensity)==rownames(msexp$pepinfo))
-  aggval = list( msexp$pepinfo$ProteinName)
-  intensity = aggregate(msexp$Intensity,by= aggval,FUN=FUN)
+  stopifnot(rownames(res$Intensity)==rownames(res$pepinfo))
+  
+  aggval = list( res$pepinfo$ProteinName)
+  intensity = aggregate(res$Intensity,by= aggval,FUN=FUN)
   res$Intensity = tomatrix(intensity)
   #x = aggregate(msexp$mz,by=aggval,function(x){x[1]})
+  #set null since there are no peptide values anymore
   res$mz = NULL
   res$rt = NULL
-  x = aggregate(msexp$score,by=aggval,mean)
+  # compute mean score per protein (TODO: - think about requant)
+  x = aggregate(res$score,by=aggval,mean)
   res$score = tomatrix(x)
-  
-  res$pepinfo = msexp$pepinfo[,-match(c("aggr_Fragment_Annotation","transition_group_id") , colnames( msexp$pepinfo ) )]
+  res$pepinfo = res$pepinfo[,-match(c("aggr_Fragment_Annotation","transition_group_id") , colnames( res$pepinfo ) )]
+  res$pepinfo = res$pepinfo[order(res$pepinfo$ProteinName),]
+
   tmp = duplicated( res$pepinfo$ProteinName ) 
   res$pepinfo = res$pepinfo[ !tmp , ]
-  stopifnot(res$pepinfo$ProteinName == rownames(res$Intensity))
-  rownames(res$pepinfo)[1:10] 
-  rownames(res$Intensity)[1:10]
   
+  stopifnot(res$pepinfo$ProteinName == rownames(res$Intensity))
   return(res)
 }
