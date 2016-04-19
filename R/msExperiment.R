7fae0db926ddaf8e44c02a4adcbdffb25c4e4b42
wolski <wewolski@gmail.com>
2016-04-19 07:44:41 +0200

    3 table version with full keys

--- R/msExperiment.R
+++ R/msExperiment.R
@@ -10,8 +10,9 @@
                     "PrecursorScore"
 )
 
-.ProteinDefs <- c("ProteinName",
-                  "StrippedSequence","IsotopeLabelType")
+.PeptideDefs <- c("ProteinName",
+                  "StrippedSequence",
+                  "Decoy")
 
 .PrecursorDefs <- c("Filename",
                     "StrippedSequence",
@@ -28,9 +29,9 @@
                   "FragmentCharge",
                   "FragmentIntensity")
 
-protein <- data.frame(unique(data[, .ProteinDefs]),stringsAsFactors = FALSE)
-precursor <- data.frame(unique(data[, .PrecursorDefs]),stringsAsFactors = FALSE)
-transition <- data.frame(unique(data[, .FragmentDefs]),stringsAsFactors = FALSE)
+#protein <- data.frame(unique(data[, .ProteinDefs]),stringsAsFactors = FALSE)
+#precursor <- data.frame(unique(data[, .PrecursorDefs]),stringsAsFactors = FALSE)
+#transition <- data.frame(unique(data[, .FragmentDefs]),stringsAsFactors = FALSE)
 
 
 sumtop <- function( x , top=3 ){
@@ -50,94 +51,125 @@ sumtop <- function( x , top=3 ){
 #' #@field columnsAll required columns for transitiondata
 #' #@field columnsPrecursor required columns for precursor
 #' @import methods
-#' @export msTransitions
-#' @exportClass msTransitions
+#' @export msTransitionExperiment
+#' @exportClass msTransitionExperiment
 #' @examples 
 #' rm(list=ls())
 #' library(imsbInfer2)
 #' library(readr)
-#' 
-#' data <- read_tsv(file,col_names = TRUE)
+#' data <- read_tsv("inst/extdata/example.tsv.gz",col_names = TRUE)
+#' dim(data)
 #' data <- prepareOpenSwathData(data)
-#' huhu <- msTransitions()
-#' huhu$setData(prepOS)
-#' huhu$getFDR()
-#' huhu$plotFDR()
+#' 
+#' huhu <- msTransitionExperiment()
+#' huhu$setData(data)
+#' intTrans <- huhu$getFragmentIntensities()
+#' precRT <- huhu$getPrecursorRT()
+#' precMZ <- huhu$getPrecursorMZ()
+#' precScore <- huhu$getPrecursorScore()
+#' dim(precRT)
+#' 
+#' head(precRT)
+#' dim(intTrans)
+#' head(intTrans)
+#' test <- (huhu$getPrecursorIntensity())
+#' dim(huhu$precursor)
+#' colnames(huhu$peptide)
+#' dim(huhu$peptide)
+#' dim(unique(huhu$peptide))
+#' head(huhu$peptide)
+#' colnames(huhu$precursor)
+#' huhu$plotGlobalFDR()
 #' decs<-huhu$getDecoy()
 
-msTransitionExperiment <- setRefClass("msTransitionsExperiment",
-                                      fields = list( 
-                                        protein = function(x){
-                                          data.frame(unique(x[, .ProteinDefs]),stringsAsFactors = FALSE)
-                                        },
-                                        precursor = function(x){
-                                          data.frame(unique(x[, .PrecursorDefs]),stringsAsFactors = FALSE)
-                                        },
-                                        transition = function(x){
-                                          data.frame(unique(data[, .FragmentDefs]),stringsAsFactors = FALSE)
-                                        }
-                                      ),
-                                      methods = list(
-                                        initialize = function(...) {
-                                          require(reshape2)
-                                        },
-                                        setData = function(data){
-                                          'set the data'
-                                          protein <<- data
-                                          precursor <<- data
-                                          transition <<- data
-                                        },
-                                        
-                                        getFragmentIntensities = function() {
-                                          'matrix with transitions intensities'
-                                          transIntensities <- dcast(transition,
-                                                                    ModifiedSequence + PrecursorCharge + FragmentIonType + FragmentCharge ~ Filename ,
-                                                                    value.var="FragmentIntensity")
-                                          return(transIntensities)
-                                        },
-                                        
-                                        getPrecursorIntensity=function(FUN=sum){
-                                          transIntensities <- getFragmentIntensities()
-                                          key <- paste(transIntensities$ModifiedSequence, ModifiedSequence$z, sep="_")
-                                          res <- by(transIntensities,INDICES=key,FUN=FUN)
-                                          do.call("rbind",res)
-                                        },
-                                        
-                                        getPrecursorRT = function() {
-                                          'matrix with precursor retention times'
-                                          transRT = dcast(precdata, ModifiedSequence + PrecursorCharge  ~ Filename , value.var="PrecursorRT")
-                                        },
-                                        
-                                        getPrecursorScore = function() {
-                                          'matrix with precursor scores'
-                                          transScore = dcast(precursor, ModifiedSequence + PrecursorCharge ~ Filename , value.var="PrecursorScore")
-                                          return(transScore)
-                                        },
-                                        getPrecursorMZ = function() {
-                                          'matrix with precursor mz'
-                                          transMz = dcast(precursor, ModifiedSequence + PrecursorCharge  ~ Filename , value.var="PrecursorMZ")
-                                          return(transMz)
-                                        },
-                                        getDecoy= function(){
-                                          'matrix with decoy information'
-                                          transDecoy = dcast(precursor, ModifiedSequence + PrecursorCharge  ~ Filename , value.var="Decoy")
-                                          return(transDecoy)
-                                        },
-                                        getGlobalFDR = function(){
-                                          'compute FDR for dataset'
-                                          tmp <- precursor[, c("ProteinName", "Decoy", "PrecursorScore")]
-                                          tmp <- tmp[tmp$Score < 2,] # not sure how to treat requant values in this context
-                                          fdr <- (sum(tmp$Decoy) / length(tmp$ProteinName))
-                                          return(fdr)
-                                        },
-                                        plotSampleFDR = function(log=""){
-                                          'plot FDR versus Score'
-                                          tmp <-data.frame(Protein = precdata$Protein,Decoy = precdata$Decoy,Score = precdata$Score)
-                                          tmp <- tmp[tmp$Score < 2,] # not sure how to treat requant values in this context
-                                          tmp <- tmp[order(tmp$Score),]
-                                          plot(tmp$Score,cumsum(tmp$Decoy) / nrow(tmp) * 100 ,type="l",xlab="Score", ylab="FDR",log=log)
-                                        }
-                                        
-                                      ))
+msTransitionExperiment <- 
+  setRefClass("msTransitionExperiment",
+              fields = list( .data="list", isotopeLabelType = "character",
+                             
+                             peptide = function(x){
+                               if(missing(x)){
+                                 return(.data$peptide)
+                               }
+                               .data$peptide <<- data.frame(unique(x[, .PeptideDefs]),stringsAsFactors = FALSE)
+                             },
+                             precursor = function(x){
+                               if(missing(x)){
+                                 return(.data$precursor)
+                               }
+                               .data$precursor <<-data.frame(unique(x[, .PrecursorDefs]),stringsAsFactors = FALSE)
+                             },
+                             transition = function(x){
+                               if(missing(x)){
+                                 return(.data$transition)
+                               }
+                               .data$transition <<-data.frame(unique(x[, .FragmentDefs]),stringsAsFactors = FALSE)
+                             }
+              ),
+              methods = list(
+                initialize = function(...) {
+                  require(reshape2)
+                  .data$peptide<<-NULL
+                  .data$precursor<<-NULL
+                  .data$transition<<-NULL
+                },
+                
+                setData = function(data, IsotopeLabelType = "L"){
+                  'set the data'
+                  data<-data[data$IsotopeLabelType=="L",]
+                  isotopeLabelType<<-"L"
+                  peptide <<- data
+                  precursor <<- data
+                  transition <<- data
+                },
+                
+                getFragmentIntensities = function() {
+                  'matrix with transitions intensities'
+                  transInt <- dcast(transition,
+                                    ModifiedSequence + PrecursorCharge + FragmentIonType + FragmentCharge ~ Filename ,
+                                    value.var="FragmentIntensity")
+                  return(transInt)
+                },
+                
+                
+                getPrecursorRT = function() {
+                  'matrix with precursor retention times'
+                  transRT = dcast(precursor, ModifiedSequence + PrecursorCharge  ~ Filename , value.var="PrecursorRT")
+                },
+                
+                getPrecursorScore = function() {
+                  'matrix with precursor scores'
+                  transScore = dcast(precursor, ModifiedSequence + PrecursorCharge ~ Filename , value.var="PrecursorScore")
+                  return(transScore)
+                },
+                getPrecursorMZ = function() {
+                  'matrix with precursor mz'
+                  transMz = dcast(precursor, ModifiedSequence + PrecursorCharge  ~ Filename , value.var="PrecursorMZ")
+                  return(transMz)
+                },
+                
+              
+                getPrecursorIntensity=function(FUN=sum){
+                  transIntensities <- getFragmentIntensities()
+                  key <- paste(transIntensities$ModifiedSequence, ModifiedSequence$z, sep="_")
+                  res <- by(transIntensities,INDICES=key)
+                  return(res)
+                },
+                
+                getGlobalFDR = function(){
+                  'compute FDR for dataset'
+                  tmp <- precursor[, c("ProteinName", "Decoy", "PrecursorScore")]
+                  tmp <- tmp[tmp$Score < 2,] # not sure how to treat requant values in this context
+                  fdr <- (sum(tmp$Decoy) / length(tmp$ProteinName))
+                  return(fdr)
+                },
+                plotSampleFDR = function(log=""){
+                  'plot FDR versus Score'
+                  tmp <-data.frame(Protein = precdata$Protein,Decoy = precdata$Decoy,Score = precdata$Score)
+                  tmp <- tmp[tmp$Score < 2,] # not sure how to treat requant values in this context
+                  tmp <- tmp[order(tmp$Score),]
+                  plot(tmp$Score,cumsum(tmp$Decoy) / nrow(tmp) * 100 ,type="l",xlab="Score", ylab="FDR",log=log)
+                }
+                
+              ))
 
 
